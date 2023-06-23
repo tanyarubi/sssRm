975f7f55749d3dd36d7b227187a37125b93d4d57
tanyarubi <tanyarubi@gmail.com>
2023-06-23 11:09:52 +0300

    wrote dotted theme function

---
+++ R/theme_dotted.R
@@ -0,0 +1,23 @@
+#' Theme Dotted - a custom ggplot2 theme
+#'
+#' @return A theme function
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' p <- ggplot(mtcars, aes(mpg, hp)) + geom_point()
+#' p + theme_dotted()
+
+
+theme_dotted <- function() {
+  ggplot2::theme_minimal() +
+    ggplot2::theme(
+      panel.grid.major.y = ggplot2::element_line(color = "grey", size = 0.1, linetype = "dotted"),
+      panel.background = ggplot2::element_blank(),
+      axis.line = ggplot2::element_line(colour = "grey"),
+      legend.title = ggplot2::element_text(size = 12),
+      text = ggplot2::element_text(face = "plain", family = "sans"),
+      legend.key = ggplot2::element_blank()
+    )
+}
